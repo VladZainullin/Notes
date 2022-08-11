f3b20bd9baf541626a3ad001e728a7d0a00c9966
Vladislav Zainullin <mittoface02@mail.ru>
2022-08-11 23:24:57 +0300

    Дополняет конфигурацию

--- Notes.Data/Contexts/AppDbContext.cs
+++ Notes.Data/Contexts/AppDbContext.cs
@@ -7,22 +7,47 @@ using Notes.Data.Visitors;
 
 namespace Notes.Data.Contexts;
 
+/// <summary>
+/// Пользовательский контекст базы данных
+/// </summary>
 public sealed class AppDbContext : DbContext
 {
+    /// <summary>
+    /// Конфигурация приложения
+    /// </summary>
     private readonly IConfiguration _configuration;
 
-    public AppDbContext(DbContextOptions<AppDbContext> options, IConfiguration configuration) :
-        base(options)
+    /// <summary>
+    /// Конструктор пользовательского контекста базы данных
+    /// </summary>
+    /// <param name="options">Опции контекста</param>
+    /// <param name="configuration">Конфигурация приложения</param>
+    public AppDbContext(
+        DbContextOptions<AppDbContext> options, 
+        IConfiguration configuration) : base(options)
     {
         _configuration = configuration;
     }
 
+    /// <summary>
+    /// Заметки
+    /// </summary>
     public DbSet<Note> Notes { get; set; } = null!;
 
+    /// <summary>
+    /// Ярлыки
+    /// </summary>
     public DbSet<Label> Labels { get; set; } = null!;
 
+    /// <summary>
+    /// Ярлыки заметок
+    /// </summary>
     public DbSet<NoteLabel> NoteLabels { get; set; } = null!;
 
+    /// <summary>
+    /// Переопределённый метод конфигурации контекста базы данных
+    /// </summary>
+    /// <param name="builder">Строитель контекста базы данных</param>
     protected override void OnConfiguring(DbContextOptionsBuilder builder)
     {
         builder.UseNpgsql(_configuration.GetConnectionString("Postgres"), o => { o.MigrationsAssembly("Notes.Web"); });
@@ -30,6 +55,10 @@ public sealed class AppDbContext : DbContext
         base.OnConfiguring(builder);
     }
 
+    /// <summary>
+    /// Переопределённый метод моделирования сущностей контекста
+    /// </summary>
+    /// <param name="modelBuilder">Строитель модели сущностей контекста</param>
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.Load("Notes.Data"));
@@ -37,16 +66,20 @@ public sealed class AppDbContext : DbContext
         base.OnModelCreating(modelBuilder);
     }
 
+    /// <summary>
+    /// Переопределённый метод сохранения данных контекста базы данных
+    /// </summary>
+    /// <param name="cancellationToken">Токен отмены</param>
+    /// <returns>Успешность сохранения</returns>
     public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
-        var entries = ChangeTracker
+        var histories = ChangeTracker
             .Entries<IHasHistory<IHistory>>()
-            .ToList();
-        foreach (var entry in entries)
-        {
-            var history = entry.Entity.Access(new HasHistoryVisitor(entry.State));
-            await AddAsync(history, cancellationToken);
-        }
+            .Select(h => h
+                .Entity
+                .Access(new HasHistoryVisitor(h.State)));
+        
+        await AddRangeAsync(histories, cancellationToken);
         
         return await base.SaveChangesAsync(cancellationToken);
     }
